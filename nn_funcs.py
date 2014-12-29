0cbc86a816227b0865cd7c92fb7e3b2cf42f48f4
karanbudhraja <karanb1@umbc.edu>
2014-12-29 10:35:42 +0530

    added capped regularization

--- nn_funcs.py
+++ nn_funcs.py
@@ -14,8 +14,9 @@ activation_funcs = {'sigmoid': T.nnet.sigmoid,
                     'cappedRelu': lambda x, cap: T.minimum(x * (x > 0), cap)}
 activation_funcs = DispatchTable(activation_funcs)
 
-# TODO: capped regularization
 regularization_funcs = {'l1': lambda x: T.mean(abs(x)),
+                        'cappedL1': lambda x, cap: T.minimum(T.mean(abs(x)), cap),
                         'l2': lambda x: T.mean(x ** 2),
+                        'cappedL2': lambda x, cap: T.minimum(T.mean(x ** 2), cap),
                         'none': lambda x: 0}
 regularization_funcs = DispatchTable(regularization_funcs)
